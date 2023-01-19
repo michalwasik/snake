fe278b5a34ee4362af69a9fb5b887fde04032768
michalwasik <michael.wasik22@gmail.com>
2023-01-19 12:00:21 +0100

    clean the code

--- main.py
+++ main.py
@@ -3,139 +3,6 @@ import random
 import termcolor
 import os
 from random import randint, choice
-# width = 40
-# height = 20
-# area = [[] for _ in range(height)]
-# for _ in range(width):
-#     area[0].append('+')
-# for x in range(1, height - 1):
-#     area[x].append('+')
-#     for _ in range(width - 2):
-#         area[x].append(' ')
-#     area[x].append('+')
-# for _ in range(width):
-#     area[-1].append('+')
-#
-#
-# def show_game():
-#     os.system('cls')
-#     for i in area:
-#         a = ''.join(i)
-#         termcolor.cprint(a, 'yellow')
-#
-#
-# def show_head(direction):
-#     if direction == [1, 0]:
-#         return '^'
-#     elif direction == [-1, 0]:
-#         return 'v'
-#     elif direction == [0, -1]:
-#         return '>'
-#     else:
-#         return '<'
-#
-#
-# def show_snake(show=True):
-#     for idx, (x, y) in enumerate(snake):
-#         if idx == 0:
-#             head = show_head(get_head_dir(snake))
-#             area[x][y] = head
-#         else:
-#             area[x][y] = '0'
-#     if trace:
-#         area[trace[0]][trace[1]] = ' '
-#     if show:
-#         show_game()
-#
-#
-# def get_head_dir(snake):
-#     head_dir = [
-#         snake[1][0] - snake[0][0],
-#         snake[1][1] - snake[0][1]
-#     ]
-#     return head_dir
-#
-#
-# def diff_a_b(point1, point2):
-#     return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
-#
-#
-# def possible_moves(snake):
-#     head = snake[0]
-#     all_points = [
-#         (head[0] + 1, head[1]),
-#         (head[0] - 1, head[1]),
-#         (head[0], head[1] + 1),
-#         (head[0], head[1] - 1)
-#     ]
-#     possible_points = [point for point in all_points if area[point[0]][point[1]] not in ['+', '0']]
-#     return possible_points
-#
-#
-# def closest_point(goal, points):
-#     closest = []
-#     distance = None
-#     if tuple(goal) in points:
-#         return goal
-#     for point in points:
-#         if not distance:
-#             distance = diff_a_b(goal, point)
-#             closest = list(point)
-#         else:
-#             curr_dist = diff_a_b(goal, point)
-#             if curr_dist < distance:
-#                 distance = curr_dist
-#                 closest = list(point)
-#     return tuple(closest)
-#
-#
-# def place_fruit():
-#     while True:
-#         apple = [randint(1, height - 1), randint(1, width - 1)]
-#         if area[apple[0]][apple[1]] == ' ':
-#             area[apple[0]][apple[1]] = '*'
-#             return apple
-#
-#
-# snake = [tuple([randint(1, height - 1), randint(1, width - 1)])]
-# init_tail = choice(possible_moves(snake))
-# snake.append(init_tail)
-# fruit = place_fruit()
-
-
-# while True:
-#     if list(snake[0]) == fruit:
-#         snake.append(tuple(trace))
-#         show_snake()
-#         trace = []
-#         fruit = place_fruit()
-#     else:
-#         options = possible_moves(snake)
-#         if not options:
-#             print(f"Sorry, you lost \n Your score:{len(snake)}")
-#             break
-#         move = closest_point(fruit, options)
-#         snake.insert(0, move)
-#         trace = snake[-1]
-#         snake = snake[:-1]
-#         show_snake()
-
-
-#
-# width = 40
-# height = 20
-# area = [[] for _ in range(height)]
-# for _ in range(width):
-#     area[0].append('+')
-# for x in range(1, height - 1):
-#     area[x].append('+')
-#     for _ in range(width - 2):
-#         area[x].append(' ')
-#     area[x].append('+')
-# for _ in range(width):
-#     area[-1].append('+')
-
-from random import choice
 
 
 def get_random_cord(height: int, width: int) -> tuple[int, int]:
@@ -143,8 +10,8 @@ def get_random_cord(height: int, width: int) -> tuple[int, int]:
 
 
 class Game:
-    WIDTH = 50
-    HEIGHT = 50
+    WIDTH = 40
+    HEIGHT = 40
 
     def __init__(self, n_snakes: int, snake_map: list[list[str]] = None):
         if not snake_map:
@@ -154,7 +21,6 @@ class Game:
         self.n_snakes = n_snakes
         self.snakes = [self.create_snake(4) for _ in range(n_snakes)]
         self.fruit = self._create_fruit()
-
 
     @classmethod
     def create_map(cls):
@@ -215,22 +81,27 @@ class Game:
             # print(snake)
             if snake.move():
                 self.snake_map[self.fruit[0]][self.fruit[1]] = snake.set_direction()
+                self.snake_map[snake.body[-1][0]][snake.body[-1][1]] = '0'
                 snake.body.append(self.fruit)
                 self.fruit = None
             else:
                 direction = snake.set_direction()
                 if not direction:
-                    for coord in snake.body:
-                        self.snake_map[coord[0]][coord[1]] = ' '
-                    self.snakes.remove(snake)
+                    self.del_snake(snake)
                 else:
                     self.snake_map[snake.body[0][0]][snake.body[0][1]] = ' '
                     snake.body = snake.body[1:]
                     change = snake.DIR_TO_DELTA[direction]
                     new_point = (snake.body[-1][0] + change[0], snake.body[-1][1] + change[1])
-                    self.snake_map[snake.body[-1][0]][snake.body[-1][1]] = '0'
-                    self.snake_map[new_point[0]][new_point[1]] = direction
-                    snake.body.append(new_point)
+                    if self.snake_map[new_point[0]][new_point[1]] != ' ':
+                        self.del_snake(snake)
+                    else:
+                        self.snake_map[snake.body[-1][0]][snake.body[-1][1]] = '0'
+                        # print(f'{new_point= }')
+                        # print(f'{direction= }')
+                        # print(f'{self.snake_map[new_point[0]][new_point[1]] = }')
+                        self.snake_map[new_point[0]][new_point[1]] = direction
+                        snake.body.append(new_point)
         if not self.fruit:
             self.fruit = self._create_fruit()
         self._print_map()
@@ -242,6 +113,11 @@ class Game:
         for i in self.snake_map:
             a = ''.join(i)
             termcolor.cprint(a, 'yellow')
+
+    def del_snake(self, snake: 'Snake'):
+        for coord in snake.body:
+            self.snake_map[coord[0]][coord[1]] = ' '
+        self.snakes.remove(snake)
 
 
 class Snake:
